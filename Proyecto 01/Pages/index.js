3f1be763d3bc198065b7c6b2f4021aeafaa96793
martinvaldatti <valdattimartin@gmail.com>
2022-09-01 18:30:06 -0300

    first commit

---
+++ Proyecto 01/Pages/index.js
@@ -0,0 +1,5 @@
+let numero1 = parseInt (prompt("Ingrese el primer valor: "));
+let numero2 = parseInt (prompt("Ingrese el segundo valor: "));
+ 
+let suma = (numero1 + numero2);
+alert( "El resultado de la suma es: " + suma) ;git
